344a20acf0a8876c94c547de1bf130d8be8f8699
yujie <seven8355@gmail.com>
2020-08-06 15:01:39 +0800

    Chore(dev): choose suitable target in tsconfig

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,40 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var _queue, _time_delay, _timer;
 Object.defineProperty(exports, "__esModule", { value: true });
 class TaskManager {
     constructor(time_delay = 1000) {
-        this.#queue = [];
-        this.#time_delay = time_delay;
-        this.#timer = setInterval(() => {
-            const task = this.#queue.shift();
+        _queue.set(this, void 0);
+        _time_delay.set(this, void 0);
+        _timer.set(this, void 0);
+        __classPrivateFieldSet(this, _queue, []);
+        __classPrivateFieldSet(this, _time_delay, time_delay);
+        __classPrivateFieldSet(this, _timer, setInterval(() => {
+            const task = __classPrivateFieldGet(this, _queue).shift();
             task && task();
-        }, this.#time_delay);
+        }, __classPrivateFieldGet(this, _time_delay)));
     }
-    #queue;
-    #time_delay;
-    #timer;
     get pendingCount() {
-        return this.#queue.length;
+        return __classPrivateFieldGet(this, _queue).length;
     }
     finish() {
-        clearInterval(this.#timer);
+        clearInterval(__classPrivateFieldGet(this, _timer));
     }
     publish(task) {
-        this.#queue.push(task);
+        __classPrivateFieldGet(this, _queue).push(task);
     }
 }
 exports.default = TaskManager;
+_queue = new WeakMap(), _time_delay = new WeakMap(), _timer = new WeakMap();
